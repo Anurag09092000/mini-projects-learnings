02a027771d295ec16175ff757e51c8af9d4861dc
Anurag Sharma <anuraguict09@gmail.com>
2024-03-01 14:51:17 +0530

    fetch and map the data

---
+++ fetchapi/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const FetchApi = () => {
+  const [storedData, setStoredData] = useState([]);
+
+  async function getapi() {
+    const api = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await api.json();
+    setStoredData(data);
+    //console.log(data);
+  }
+
+  // getapi();
+  return (
+    <div>
+      <h1>Api Data</h1>
+      <button onClick={getapi}> Clik ME</button>
+      {/* {storedData.length > 0 ? (
+        storedData.map(function (item, index) {
+          return (
+            <h2>
+              {index + 1} 👉 Title : {item.title}
+            </h2>
+          );
+        })
+      ) : (
+        <h1>There is not data yet..please fetch from button</h1>
+      )} */}
+      {storedData.length > 0 &&
+        storedData.map(function (item, index) {
+          return (
+            <h2>
+              {index + 1} 👉 Title : {item.title}
+            </h2>
+          );
+        })}
+      {storedData.length === 0 && (
+        <h1>There is not data yet..please fetch from button</h1>
+      )}
+    </div>
+  );
+};
+
+export default FetchApi;
