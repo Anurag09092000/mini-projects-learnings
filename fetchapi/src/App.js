9dd8424955e012b09568c686973bb273e787c60c
Anurag Sharma <anuraguict09@gmail.com>
2024-03-01 15:52:40 +0530

    showing loading while data is being fetched

--- fetchapi/src/App.js
+++ fetchapi/src/App.js
@@ -1,21 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const FetchApi = () => {
   const [storedData, setStoredData] = useState([]);
+  const [isLoding, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   getapi();
+  // }, []);
 
   async function getapi() {
+    setIsLoading(true);
     const api = await fetch("https://jsonplaceholder.typicode.com/posts");
     const data = await api.json();
     setStoredData(data);
+    setIsLoading(false);
+
     //console.log(data);
   }
 
   // getapi();
   return (
     <div>
-      <h1>Api Data</h1>
-      <button onClick={getapi}> Clik ME</button>
-      {/* {storedData.length > 0 ? (
+      {isLoding ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1>Api Data</h1>
+          <button onClick={getapi}> Clik ME</button>
+          {/* {storedData.length > 0 ? (
         storedData.map(function (item, index) {
           return (
             <h2>
@@ -26,16 +38,18 @@ const FetchApi = () => {
       ) : (
         <h1>There is not data yet..please fetch from button</h1>
       )} */}
-      {storedData.length > 0 &&
-        storedData.map(function (item, index) {
-          return (
-            <h2>
-              {index + 1} 👉 Title : {item.title}
-            </h2>
-          );
-        })}
-      {storedData.length === 0 && (
-        <h1>There is not data yet..please fetch from button</h1>
+          {storedData.length > 0 &&
+            storedData.map(function (item, index) {
+              return (
+                <h2>
+                  {index + 1} 👉 Title : {item.title}
+                </h2>
+              );
+            })}
+          {storedData.length === 0 && (
+            <h1>There is not data yet..please fetch from button</h1>
+          )}
+        </div>
       )}
     </div>
   );
